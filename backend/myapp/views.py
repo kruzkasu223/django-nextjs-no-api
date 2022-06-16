17f881a2b7d6be17bd19fd89d3e30982ddf95926
Krushang Kasundra <kruzkasu223@gmail.com>
2022-06-16 18:58:43 +0530

    firsr and final commit

---
+++ backend/myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Posts
+# Create your views here.
+
+
+def posts_list(request):
+    posts = Posts.objects.all()
+    return render(request, 'posts.html', {"posts": posts})
